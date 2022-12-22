160053070a21a3b930f875d88ba41bd625c03f01
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-22 15:33:39 +0200

    Add project files.

---
+++ SearchingAndSubstringExtracting/FormSearchingAndSubstringExtracting.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SearchingAndSubstringExtracting
+{
+    partial class FormSearchingAndSubstringExtracting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnProblem1 = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.BtnProblem2 = new System.Windows.Forms.Button();
+            this.BtnProblem3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnProblem1
+            // 
+            this.BtnProblem1.Location = new System.Drawing.Point(36, 42);
+            this.BtnProblem1.Name = "BtnProblem1";
+            this.BtnProblem1.Size = new System.Drawing.Size(189, 59);
+            this.BtnProblem1.TabIndex = 0;
+            this.BtnProblem1.Text = "Problem 1";
+            this.BtnProblem1.UseVisualStyleBackColor = true;
+            this.BtnProblem1.Click += new System.EventHandler(this.BtnProblem1_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(258, 42);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(462, 361);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // BtnProblem2
+            // 
+            this.BtnProblem2.Location = new System.Drawing.Point(36, 127);
+            this.BtnProblem2.Name = "BtnProblem2";
+            this.BtnProblem2.Size = new System.Drawing.Size(189, 59);
+            this.BtnProblem2.TabIndex = 2;
+            this.BtnProblem2.Text = "Problem 2";
+            this.BtnProblem2.UseVisualStyleBackColor = true;
+            this.BtnProblem2.Click += new System.EventHandler(this.BtnProblem2_Click);
+            // 
+            // BtnProblem3
+            // 
+            this.BtnProblem3.Location = new System.Drawing.Point(36, 208);
+            this.BtnProblem3.Name = "BtnProblem3";
+            this.BtnProblem3.Size = new System.Drawing.Size(189, 62);
+            this.BtnProblem3.TabIndex = 3;
+            this.BtnProblem3.Text = "Problem 3";
+            this.BtnProblem3.UseVisualStyleBackColor = true;
+            this.BtnProblem3.Click += new System.EventHandler(this.BtnProblem3_Click);
+            // 
+            // FormSearchingAndSubstringExtracting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnProblem3);
+            this.Controls.Add(this.BtnProblem2);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnProblem1);
+            this.Name = "FormSearchingAndSubstringExtracting";
+            this.Text = "Searching and Substring Extracting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnProblem1;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.Button BtnProblem2;
+        private System.Windows.Forms.Button BtnProblem3;
+    }
+}
+
---
+++ SearchingAndSubstringExtracting/FormSearchingAndSubstringExtracting.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace SearchingAndSubstringExtracting
+{
+    public partial class FormSearchingAndSubstringExtracting : Form
+    {
+        public FormSearchingAndSubstringExtracting()
+        {
+            InitializeComponent();
+        }
+
+        int wordsCount = 0;
+
+        private void BtnProblem1_Click(object sender, EventArgs e)
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Enter text: ", "Text");
+            string character = Microsoft.VisualBasic.Interaction.InputBox("Enter a character: ", "Character");
+
+            //We separate each word and add it to the array.
+            string[] split = SplitString(input, Convert.ToChar(character), ref wordsCount);
+
+
+            for (int i = split.Length - 1; i >= 0 ; i--)
+            {
+                if (split[i] != null)
+                {
+                    TxtResult.Text += split[i];
+
+                    if (i != 0)
+                    {
+                        TxtResult.Text += " ";
+                    }
+                }
+            }
+        }
+
+        private void BtnProblem2_Click(object sender, EventArgs e)
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Enter a phone number: ", "Phone");
+            string character = Microsoft.VisualBasic.Interaction.InputBox("Enter a character: ", "Character");
+
+            string[] split = SplitString(input, Convert.ToChar(character), ref wordsCount);
+
+
+            TxtResult.Text += "Area code is: " + split[0] + Environment.NewLine;
+            TxtResult.Text += "Phone number is: ";
+
+            for (int i = 1; i < split.Length - 1; i++)
+            {
+                if (split[i] != null)
+                {
+                    TxtResult.Text += split[i];
+
+                    if (i != wordsCount)
+                    {
+                        TxtResult.Text += "-";
+                    }
+                }
+            }
+        }
+
+        private void BtnProblem3_Click(object sender, EventArgs e)
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Enter a decimal number: ", "Number");
+
+            int decimalPlaces;
+            int integer;
+            int fraction;
+
+            decimalPlaces = input.IndexOf('.');
+            integer = Convert.ToInt32(input.Substring(0, decimalPlaces));
+            fraction = Convert.ToInt32(input.Substring(decimalPlaces + 1));
+
+            TxtResult.Text += "The number of decimal places is: " + decimalPlaces + Environment.NewLine;
+            TxtResult.Text += "The integer part is: " + integer + Environment.NewLine;
+            TxtResult.Text += "The decimal part is: " + fraction;
+
+        }
+
+        private string[] SplitString(string input, char character, ref int wordsCount)
+        {
+            char tempChar;
+            string[] words = new string[100];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                tempChar = Convert.ToChar(input.Substring(i, 1));
+
+                if (tempChar == character)
+                {
+                    wordsCount++;
+                }
+                else
+                {
+                    words[wordsCount] += tempChar.ToString();
+                }
+            }
+            return words;
+        }
+
+    }
+}
